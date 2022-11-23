ad40f38856ff60dfce95794b4ebb15dceef48782
Luke Barkess <luke.barkess@icloud.com>
2022-11-23 12:39:17 +0000

    Add project files.

---
+++ Prime Numbers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int i = 1; i < 1000; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }*/
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (IsPrime(input))
+            {
+                Console.WriteLine("Prime");
+            }
+
+            else
+            {
+                Console.WriteLine("Not Prime");
+            }
+
+            Console.ReadLine();
+        }
+
+        static bool IsPrime(int number)
+        {
+            if (number == 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
