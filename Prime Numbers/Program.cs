d8b330b122c4a7ec44f09d10f81e3bca88fa3f8a
Luke Barkess <luke.barkess@icloud.com>
2022-11-28 15:19:27 +0000

    asd

--- Prime Numbers/Program.cs
+++ Prime Numbers/Program.cs
@@ -3,32 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Prime_Numbers
 {
     class Program
     {
+        static Stopwatch stopwatch = new Stopwatch();
+
         static void Main(string[] args)
         {
-            for (int i = 1; i < 10000; i++)
-            {
-                if (IsPrime(i))
-                {
-                    Console.WriteLine(i);
-                }
-            }
+
+            int primes = 0;
+
+            stopwatch.Start();
+
+            TestInThreads(2, 0, 1000000);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"There are {primes} primes between {minimum} and {maximum}");
+            Console.WriteLine($"Took {stopwatch.Elapsed} time");
 
             Console.ReadLine();
         }
 
+        static void TestInThreads(int numberOfThreads, int minimum, int maximum)
+        {
+            int primes = 0;
+            Task[] tasks = new Task[numberOfThreads];
+
+            for (int j = 0; j < numberOfThreads; j++)
+            {
+                tasks[j] = Task.Run(() =>
+                {
+                    for (int i = (maximum / numberOfThreads) * j; i < (maximum / numberOfThreads) * (j + 1); i++)
+                    {
+                        if (IsPrime(i))
+                        {
+                            primes++;
+                        }
+                    }
+                });
+            }
+        }
+
         static bool IsPrime(int number)
         {
-            if (number == 1)
+            switch (number)
+            {
+                case 0:
+                    return false;
+                case 1:
+                    return false;
+                case 2:
+                    return true;
+            }
+
+            if (number % 2 == 0)
             {
                 return false;
             }
 
-            for (int i = 2; i < number; i += 2)
+            double numberSquareRoot = Math.Sqrt(number);
+
+            for (int i = 3; i <= numberSquareRoot; i += 2)
             {
                 if (number % i == 0)
                 {
